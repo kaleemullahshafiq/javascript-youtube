bab6b1a9435bde892674df1c29720a9f1791645b
kaleemullahshafiq <153160090+kaleemullahshafiq@users.noreply.github.com>
2023-12-09 05:13:27 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+                            // this is the best converson beacuse number in the both side
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2!=1);
+// console.log(2==1);
+                            // it's not good, one side is string and other is number
+// console.log("2" > 1);
+// console.log("02" > 1);
+                           
+                            // avoide from this type of conversion, beacuse in some case it come true or false 
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);     // the true means in (3) null convert to 0
+
+// console.log(undefined >= 0);     
+// console.log(undefined >= 0);     
+console.log(2 === 2);     
+console.log("2" === 2);     
+// two important things (1) equality check like ==, ===  (2) comparison
+// === it's called streck cheeck, it cheecks the number and datatype both. first->true, second->false
